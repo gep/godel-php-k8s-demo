b1a438809195616bd178ca7053a570515e992549
Andrey Semikov <andreysemikov@rentalcars.com>
2019-06-19 18:19:05 +0300

    Controller update

--- src/Controller/GoodsController.php
+++ src/Controller/GoodsController.php
@@ -2,25 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Good;
+use ProbablyRational\RandomNameGenerator\All;
+use ProbablyRational\RandomNameGenerator\Alliteration;
+use ProbablyRational\RandomNameGenerator\Sketch;
+use ProbablyRational\RandomNameGenerator\Vgng;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class GoodsController extends AbstractController
 {
     /**
-    * @Route("/goods")
-    */
-    public function getGoods()
+     * @Route("/goods", name="goods")
+     */
+    public function index(): Response
     {
-        return $this->json();
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/GoodsController.php',
+        ]);
     }
 
     /**
-     * @Route("/goods/{id}")
-     * @param int $id
+     * @Route("/goods/create/{amount}", name="create_goods")
+     * @param int $amount
+     * @return Response
      */
-    public function getGoodsById(int $id)
+    public function createGoods(int $amount): Response
     {
+        $namesGenerator = new All(
+            [
+                new Alliteration(1),
+                new Vgng(1),
+                new Sketch(1),
+            ]
+        );
 
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        foreach (range(1, $amount) as $itemNumber) {
+            $product = new Good();
+            $product->setName($namesGenerator->getName());
+            $product->setPrice((float)(rand(0, 200) + (rand(0, 100) / 100)));
+
+            $entityManager->persist($product);
+        }
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+
+        return $this->json(['message' => sprintf("%d goods created", $amount)]);
     }
+
+
+    
+
 }
